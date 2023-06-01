5047c8328dfffc11dda32c343d7310200646d4dc
Simone Carcagnì <simonecarcagni@libero.it>
2023-06-01 19:56:06 +0200

    Aggiunta di risposta in caso di slug errato

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -22,9 +22,16 @@ class ProjectController extends Controller
     {
         $project = Project::where('slug', $slug)->with(['type', 'technologies'])->first();
 
-        return response()->json([
-            'success' => true,
-            'project' => $project
-        ]);
+        if ($project) {
+            return response()->json([
+                'success' => true,
+                'project' => $project
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Post non trovato!'
+            ]);
+        }
     }
 }
