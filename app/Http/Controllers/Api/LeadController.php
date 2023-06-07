0c7fc3424eafe40a3e8bc473e4885a60120694cd
Simone Carcagnì <simonecarcagni@libero.it>
2023-06-07 17:36:24 +0200

    Aggiunta di validazione e abbellimento email

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -7,16 +7,45 @@ use App\Mail\NewContact;
 use App\Models\Lead;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
     public function store(Request $request)
     {
+
         $data = $request->all();
+
+        $validator = Validator::make(
+            $data,
+            [
+                'name' => 'required',
+                'email' => 'required|email',
+                'message' => 'required'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'errors' => $validator->errors()
+                ]
+            );
+        }
+
         $newLead = new Lead();
         $newLead->fill($data);
         $newLead->save();
 
-        Mail::to('simonecarcagni87@gmail.com')->send(new NewContact($newLead));
+
+
+        Mail::to('info@simonegiusti.it')->send(new NewContact($newLead));
+
+        return response()->json(
+            [
+                'success' => true
+            ]
+        );
     }
 }
